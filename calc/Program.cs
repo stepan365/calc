e0d945868e10569c95ad3a8c5f4496667b34b8af
stepan365 <143639328+stepan365@users.noreply.github.com>
2023-09-05 21:23:47 +0300

    Добавьте файлы проекта.

---
+++ calc/Program.cs
@@ -0,0 +1,39 @@
+﻿System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Привіт у калькуляторі))");
+
+double firstValue, secondValue;
+
+string dia;
+
+Console.WriteLine("Введи цифру 1");
+firstValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Виберіть дію '+' '-' '/' '*'");
+dia = Console.ReadLine();
+
+Console.WriteLine("Введи цифру 2");
+secondValue = double.Parse(Console.ReadLine());
+Console.WriteLine("Ваш результат = ");
+
+switch (dia)
+{
+    case "+":
+        Console.WriteLine(firstValue + secondValue);
+        break;
+    case "-":
+        Console.WriteLine(firstValue - secondValue);
+        break;
+    case "*":
+        Console.WriteLine(firstValue * secondValue);
+        break;
+    case "/":
+        Console.WriteLine(firstValue / secondValue);
+        break;
+
+    default:
+        Console.WriteLine("Невідома дія");
+        break;
+
+}
+Console.WriteLine();
